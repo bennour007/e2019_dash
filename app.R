e4ad906390ca4db7dbf13347e3f318ffa23e8525
bennour007 <hsin.bennour@gmail.com>
2021-07-17 23:53:53 +0100

    published dash

--- app.R
+++ app.R
@@ -1,19 +1,46 @@
-
 library(shiny)
+library(shinydashboard)
 library(bslib)
+library(gt)
+library(gfonts)
+
 
 source('resources.R')
 source('ggtheme.R')
 
-ui <- fluidPage(
-    # adding theme wit h bslib
+
+ui <- dashboardPage(
+  skin = 'black',
+  dashboardHeader(title = '3ATABA DASHBOARD'),
+  dashboardSidebar(disable = T),
+  dashboardBody(
     
-    theme = bs_theme(version = 4, bootswatch = "minty"),
-    # Application title
-    titlePanel("How much of a threshold is needed to form a majority in the parliament?"),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+    ),
+    
     fluidRow(
-        column(5),
-        column(6,
+      column(3),
+      column(6,
+             box(
+               width = NULL,
+               status = 'primary',
+               title = 'LE THERESHOLD!',
+               h3('In this interactive app we aim to visualize the effect of changing the minimum percentage of the total votes aquired in a specific juristiction for each party on the overall look of the parliament.'),
+               footer = 'If I recieve enough feedback on this app I may also add the effect on the results for each juristiction',
+               h3('Use the slider below to set up the threshold you desire to observe, for example if you want to set the value to 2% slide the pointer to the value 0.02') 
+             )
+            )
+      
+      
+    ),
+    
+    fluidRow(
+      column(3),
+      column(6,
+             box(
+               status = 'primary',
+               width = NULL,
                h3(''),
                h3(''),
                h1(''),
@@ -22,51 +49,81 @@ ui <- fluidPage(
                            min = 0,
                            max = 0.2,
                            value = 0)
-        ),
-        column(5)
+             )
+             ),
+      column(5)
     ),
     
     fluidRow(
-        column(3),
-        column(6,
+      column(1),
+      column(7,
+             box(
+               width = NULL,
+               h1(' '),
                h3("The corresponding parliament:"),
-               plotOutput("seats")
-        ),
-        column(3)
+               h1(''),
+               plotOutput("seats"),
+               h1(' '),
+               h3('Comparaison between the chosen & the current threshold for the top 7 parties:'),
+               h1(''),
+               plotOutput("comp")
+             )
+      ),
+      column(3,
+             box(
+               width = NULL,
+               h1(' '),
+               h3('The percentage of the direct votes(quota) seats to the total seats'),
+               h1(''),
+               tableOutput("ratio")
+             )
+      ),
+      column(1)
     )
-    # Sidebar with a slider input for number of bins 
-    # sidebarLayout(
-    #     sidebarPanel(
-    #         sliderInput("percent",
-    #                     "Threshhold on the percentqge of votes on every juristiction",
-    #                     min = 0,
-    #                     max = 0.2,
-    #                     value = 0)
-    #     ),
-    #     
-    #     # Show a plot of the generated distribution
-    #     mainPanel(
-    #         plotOutput("seats")
-    #     )
-    # )
+  )
 )
+    
+    
+  
+
 
 
 server <- function(input, output) {
+  
+  thematic::thematic_shiny()
+  
+  output$seats <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    data_to_use <- good_data(data_ready, x = input$percent)  
     
-    output$seats <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        data_to_use <- good_data(data_ready, x = input$percent)  
-        
-        data_to_use %>% 
-            mutate(party = fct_reorder(party, total_seats)) %>% 
-            ggplot() +
-            geom_col(aes(x = total_seats,
-                         y = party)) + 
-            theme_light()
-        
-    })
+    data_to_use %>% 
+      mutate(party = fct_reorder(party, total_seats)) %>% 
+      ggplot() +
+      geom_col(aes(x = total_seats,
+                   y = party), 
+               fill = "skyblue") +
+      labs(title = 'The effect of your chosen threshold on the overall look of the parliament',
+           y = 'Parties',
+           x = 'Total number of seats') +
+      dash_theme
+    
+  })
+  output$comp <- renderPlot({
+    j_c(per = input$percent)
+  })
+  
+  output$ratio <- renderTable({
+    
+    data_to_use <- good_data(data_ready, x = input$percent)
+    
+    data_to_use %>%
+      mutate(diff = (total_seats - r_seats)/total_seats,
+             diff = 100*round(diff, 2)) %>%
+      arrange(-diff) %>% 
+      gt() 
+  })
 }
+
 
 shinyApp(ui, server)
 
--- resources.R
+++ resources.R
@@ -1,10 +1,25 @@
 library(tidyverse)
 library(janitor)
-library(ggpol)
+
 
 
 data <- readxl::read_xlsx('data/general_results_2019.xlsx')
 seats <- readxl::read_xlsx('data/seats.xlsx')
+
+dash_theme <- theme(text = element_text(family = "DejaVu Sans Mono",
+                                        color = "black",
+                                        margin = margin(t = 10)),
+                    panel.grid.major.y = element_blank(),
+                    panel.grid.minor.y = element_blank(),
+                    strip.background = element_blank(),
+                    axis.ticks.x  = element_blank(),
+                    axis.ticks.y  = element_blank(),
+                    plot.background = element_rect(fill = "#f5ffff"),
+                    legend.background = element_rect(fill = "#f5ffff"),
+                    legend.box.background = element_rect(fill = "#f5ffff", colour = NA),
+                    legend.position ="bottom",
+                    panel.background = element_rect(fill= "#f5ffff"),
+                    plot.margin = margin(10, 100, 10, 100))
 
 
 zirou <- function(x){
@@ -28,7 +43,9 @@ data_ready <- data_clean %>%
                values_to = 'votes',
                names_to = 'party') %>% 
   rename('state'='name_fr') %>% 
-  left_join(seats, by = 'state')
+  left_join(seats, by = 'state') %>% 
+  mutate(party = str_replace_all(party, '_',' '),
+         party = str_to_upper(party))
 
 
 good_data <- function(data, x){
@@ -39,7 +56,7 @@ good_data <- function(data, x){
     mutate(total_votes = sum(votes),
            percent = votes/total_votes) %>% 
     filter(percent >= x) %>% 
-    filter(party != 'autres') %>% 
+    filter(party != 'AUTRES') %>% 
     mutate(hare_quota = total_votes/n_of_seats,
            party_quota = votes/hare_quota,
            quota_seats = as.integer(party_quota),
@@ -78,14 +95,44 @@ good_data <- function(data, x){
 }
 
 
+# comparaison plot
 
-
-
-
-
-
-
-
+j_c <- function(per){
+  
+  good_data(data_ready, 0) %>% 
+    left_join(good_data(data_ready, x = per), 
+              by = "party") %>% 
+    mutate_all(.funs = zirou) %>% 
+    rename('initial_quota' = "q_seats.x",
+           'new_quota' = "q_seats.y",
+           'initial_remains' = "r_seats.x",
+           'initial_total' = "total_seats.x",
+           'new_remains' = "r_seats.y",
+           'new_total' = "total_seats.y") %>% 
+    select(- initial_total, -new_total) %>% 
+    pivot_longer(cols = initial_quota:new_remains,
+                 values_to = 'seats',
+                 names_to = 'nature') %>% 
+    mutate(comp = if_else(str_detect(nature, 'initial') == T, 'Initial', 'New'),
+           # comp = if_else(str_detect(nature, 'quota') == T, 'Quota', comp),
+           nature = str_remove_all(nature, 'initial_'),
+           nature = str_remove_all(nature, 'new_')) %>% 
+    mutate(party = fct_lump(party, w = seats, 7)) %>%
+    mutate(party = fct_reorder(party, seats)) %>%
+    ggplot() +
+    geom_col(aes(x = seats,
+                 y = party,
+                 fill = nature)
+    ) +
+    scale_fill_manual(values=c("skyblue", "#3a5f63"), 
+                      name="Decomposition:",
+                      breaks=c("quota", "remains"),
+                      labels=c("Direct votes seats(quota)", "Remains seats"))+
+    facet_grid(~ comp) +
+    labs(title = 'How the total aquired seats are decomposed between direct votes and largest remains:')+
+    dash_theme %>% 
+    return()
+}
 
 
 
